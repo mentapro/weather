1a171e843a19124740945c226674a3e92f68c529
Stanislav Herasymiuk <herasymiuk.s@nimses.com>
2019-05-24 21:21:56 +0300

    Initial commit

---
+++ src/Weather.Api/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class WeatherController : ControllerBase
+	{
+		public WeatherController()
+		{
+		}
+	}
+}
